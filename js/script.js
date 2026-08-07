61a4ff073cbb0b2784ebb45b2f9676110370be90
tamago-01 <ikuko.1102.iw@gmail.com>
2026-08-07 12:06:38 +0900

    fb2_修正

--- js/script.js
+++ js/script.js
@@ -9,61 +9,9 @@ $(".p-header__nav-menu a").on("click", function () {
   $(".p-header__nav-menu").removeClass("is-open");
 });
 
-// スワイパー
-const swiper = new Swiper(".swiper", {
-  // Optional parameters
-  // direction: 'vertical',
-  loop: true,
-  slidesPerView: 1,
 
-  breakpoints: {
-    768: {
-      slidesPerView: 3,
-      spaceBetween: 35,
-    },
-  },
 
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-  },
 
-  // Navigation arrows
-
-  navigation: {
-    nextEl: ".gallery__next",
-    prevEl: ".gallery__prev",
-  },
-});
-
-// アコーディオン;
-// jQuery(".js-accordion").on("click", function (e) {
-//   e.preventDefault();
-
-//   if (jQuery(this).parent().hasClass("is-open")) {
-//     jQuery(this).parent().removeClass("is-open");
-//     jQuery(this).next().slideUp();
-//   } else {
-//     jQuery(this).parent().addClass("is-open");
-//     jQuery(this).next().slideDown();
-//   }
-// });
-
-// アコーディオン
-jQuery(".js-accordion").on("click", function (e) {
-  e.preventDefault();
-
-  const box = jQuery(this).closest(".p-qa__box"); // 属するboxを取得
-  const answer = box.find(".p-qa__a-box"); // その中の回答ボックス
-
-  if (box.hasClass("is-open")) {
-    box.removeClass("is-open");
-    answer.slideUp();
-  } else {
-    box.addClass("is-open");
-    answer.slideDown();
-  }
-});
 
 // トップへ戻るボタン
 $(window).on("scroll", function () {
